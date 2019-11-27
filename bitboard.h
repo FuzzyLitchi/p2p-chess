344c220233e308cc4edf8bc3f0dcf47dab4985bc
Polly Nielsen Boutet-Livoff <pollyboutet@gmail.com>
2019-11-27 23:48:12 +0100

    Add & | ^ operators to BitBoard

--- bitboard.h
+++ bitboard.h
@@ -7,7 +7,23 @@ class BitBoard {
 public:
   u64 value;
 
+  BitBoard(u64 v) {
+    value = v;
+  }
+
   void print();
+
+  BitBoard operator & (BitBoard other) {
+    return BitBoard(value & other.value);
+  };
+
+  BitBoard operator | (BitBoard other) {
+    return BitBoard (value | other.value);
+  };
+
+  BitBoard operator ^ (BitBoard other) {
+    return BitBoard(value ^ other.value);
+  };
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@
 int main() 
 {
     BitBoard bitboard { 18446462598732906495 };
+    BitBoard filter { 89888890099 };
+
+    BitBoard stuff = filter & bitboard;
 
     bitboard.print();
+    std::cout << "\n";
+    filter.print();
+    std::cout << "\n";
+    stuff.print();
     return 0;
 }
